6e8af8f7deafb5d5351151f24b507b880a3d3f06
Password last changed on 2019-03-29 <root@USSLTCCSL2416.solutions.glbsnet.com>
2019-12-17 11:21:59 -0500

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Let me try to change this!
         </p>
         <a
           className="App-link"
